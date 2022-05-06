819d9f825f8a9f98ea8757b84981ae9505396a6a
Андрей Кожев <andrey.kozhev.2010@outlook.com>
2022-05-06 20:57:16 +0300

    Update clocks.bat

--- bat-example/clocks.bat
+++ bat-example/clocks.bat
@@ -1,9 +1,9 @@
 @echo off
 @title Clocks
 @mode con: cols=30 lines=5
-color 1a
+color 1c
 :path
-echo  Time %time:~0,-3%
+echo Time: %time:~0,-3%
 rem Пингуем для задержки в программе
 ping 127.0.0.1 -n 2 >nul
 cls
